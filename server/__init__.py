7a9e01ab71b37549d2a92c4c8da0f42be176f6c3
gurman singh <gurmansingh3030@gmail.com>
2026-04-08 16:02:24 +0530

    Move OpenEnv files to root

---
+++ server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Patient Triage Env environment server components."""
+
+from .patient_triage_env_environment import PatientTriageEnvironment
+
+__all__ = ["PatientTriageEnvironment"]
